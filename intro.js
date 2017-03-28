5d88bc3be712e5e48029cca84562d82a8d04f57f
Muhammet <muhammeta7@gmail.com>
2017-03-28 14:18:44 -0400

    finished rock paper scissors

--- intro.js
+++ intro.js
@@ -60,7 +60,7 @@ if (quarter(12) % 3 == 0) {
   console.log("This statement is true");
 } else {
   console.log("This statement is false");
-};
+}
 
 // Functions with two parameters
 var perimeterBox = function (width, length) {
@@ -104,3 +104,49 @@ console.log(sleepCheck(10));
 console.log(sleepCheck(5));
 console.log(sleepCheck(8));
 
+// Rock, Paper, Scissors
+var userChoice = prompt("Do you choose rock, paper or scissors?");
+if (userChoice != "rock" || userChoice != "paper" || userChoice != "scissors") {
+    userChoice = prompt("You need to choose rock, paper, or scissors");
+}
+
+var computerChoice = Math.random();
+if (computerChoice < 0.34) {
+  computerChoice = "rock";
+} else if(computerChoice <= 0.67) {
+  computerChoice = "paper";
+} else {
+  computerChoice = "scissors";
+} console.log("Computer: " + computerChoice);
+
+var compare = function(userChoice,computerChoice) {
+    if (userChoice == computerChoice) {
+        return "The result is a tie!"
+    }
+    else if (userChoice == "rock"){
+        if (computerChoice == "scissors"){
+            return "rock wins";   
+        }
+        else {
+            return "paper wins"
+        }
+    }
+    else if (userChoice == "paper"){
+        if (computerChoice == "rock") {
+            return "paper wins";
+        }
+        else {
+            return "scissors wins";
+        }
+    }
+    else {
+        if (computerChoice == "rock"){
+            return "rock wins";
+        }
+        else {
+            return "scissors wins";
+        }
+    }
+}
+
+ compare(userChoice, computerChoice);
