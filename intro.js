a4a47aadf9b4abdec343ef9509a1b207d63370a0
Muhammet <muhammeta7@gmail.com>
2017-03-28 12:48:21 -0400

    introduction lesson complete

--- intro.js
+++ intro.js
@@ -43,4 +43,64 @@ var orangeCost = function(cost) {
 
 orangeCost(5);
 
+// Return keyword
+var twoTwimes = function(number) {
+  return number*2;
+};
+
+var newNumber = twoTimes(3);
+console.log(newNumber);
+
+// Function called quarter
+var quarter = function(number) {
+  return number/4;
+};
+
+if (quarter(12) % 3 == 0) {
+  console.log("This statement is true");
+} else {
+  console.log("This statement is false");
+};
+
+// Functions with two parameters
+var perimeterBox = function (width, length) {
+  return 2*width + 2*length;
+};
+
+perimeterBox(3,4);
+
+// Global variables are variables that can be accessed inside any function
+// Local variables can only be called upon within functions
+var my_number = 7; //this has global scope
+
+var timesTwo = function(number) {
+    var my_number = number * 2;
+    console.log("Inside the function my_number is: ");
+    console.log(my_number);
+}; 
+
+timesTwo(7);
+console.log("Outside the function my_number is: ")
+console.log(my_number);
+
+// Functions Recap
+var nameString = function (name) {
+  return "Hi, I am " + name;
+};
+
+console.log(nameString("muhammet"));
+
+// Functions & if/else
+var sleepCheck = function(numHours) {
+    if(numHours >= 8 ){
+        return "You're getting plenty of sleep! Maybe even too much!";
+    }
+    else {
+        return "Get some more shut eye!";
+    };
+};
+
+console.log(sleepCheck(10));
+console.log(sleepCheck(5));
+console.log(sleepCheck(8));
 
