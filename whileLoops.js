b4c0d16a74bed0dc0c1244fa70bafe9538b850b6
Muhammet <muhammeta7@gmail.com>
2017-04-04 21:48:49 -0400

    review and while/do loops

--- whileLoops.js
+++ whileLoops.js
@@ -24,10 +24,63 @@ var count = 0;
 
 var loop = function(){
   while(count<3){
-    //Your code goes here!
     count++;
     console.log("I'm looping!");
   }
 };
 
 loop();
+
+// Solo Loop
+var bool=true;
+
+var soloLoop = function(){
+  while(bool){
+    console.log("Looped once!");
+    bool=false;
+  }
+};
+
+soloLoop();
+
+// When to while and when to for loop
+for (var i=0; i<10 ; i++){
+    console.log(i);
+}
+
+var bool=2;
+while (bool < 10){
+    bool++;
+    console.log(bool);
+}
+
+// Do/While loop is hen you want to make sure your loop runs at least once
+var loopCondition = false;
+
+do {
+  console.log("I'm gonna stop looping 'cause my condition is " + loopCondition + "!");  
+} while (loopCondition);
+
+// Get to the choppa
+var getToDaChoppa = function(){
+  bool=false;
+  do {
+      console.log("nothing");
+  } while(bool)
+};
+
+getToDaChoppa();
+
+// Review
+var getToDaChoppa = function(){
+  bool=false;
+  do {
+      console.log("nothing");
+  }while(bool)
+};
+
+getToDaChoppa();
+
+for(var i=0; i<10; i++){
+    console.log(i);
+}
