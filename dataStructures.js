f6ac1c1112c9689dd9d4e5899739cb1aa1db6a5c
Muhammet <muhammeta7@gmail.com>
2017-04-12 13:29:07 -0400

    intro to data structures, basic list review

---
+++ dataStructures.js
@@ -0,0 +1,17 @@
+// Access by offset
+var languages = ["HTML", "CSS", "JavaScript", "Python", "Ruby"];
+
+console.log(languages[2]);
+console.log(languages.length);
+
+// Iterating over an array
+for (var i=0; i<languages.length; i++) {
+    console.log(languages[i]);
+}
+
+// Heterogeneous arrays (mixes data types)
+var mix = [42, true, "towel"];
+
+// An array of arrays 3x3
+var newArray = [[2,3,4],[4,4,4],[4,5,6]];
+
