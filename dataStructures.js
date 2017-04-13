0ef44a7c7dabb22597ba60d58f82f3b500663b09
Muhammet <muhammeta7@gmail.com>
2017-04-12 23:55:34 -0400

    javascripts object constructors, key value pairs

--- dataStructures.js
+++ dataStructures.js
@@ -15,3 +15,42 @@ var mix = [42, true, "towel"];
 // An array of arrays 3x3
 var newArray = [[2,3,4],[4,4,4],[4,5,6]];
 
+// -----------------------------------------------------------------------------------
+// Objects consist of key value pairs
+var phonebookEntry = {};
+
+phonebookEntry.name = 'Oxnard Montalvo';
+phonebookEntry.number = '(555) 555-5555';
+phonebookEntry.phone = function() {
+  console.log('Calling ' + this.name + ' at ' + this.number + '...');
+};
+
+phonebookEntry.phone();
+
+// Me object
+var me = {
+    name: "Muhammet",
+    age: 27
+};
+
+console.log(me);
+// Object Constructor
+var me = new Object();
+
+me.name = "Muhammet";
+me.age = 27;
+
+console.log(me);
+
+// Create 3 Objects
+var object1 = new Object();
+var object2 = new Object();
+var object3 = new Object();
+
+object1.name = "Moe";
+object2.name = "Hannah";
+object3.name = "Justin";
+
+object1.age = 27;
+object2.age = 22;
+object3.age = 19;
