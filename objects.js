7d59063a49d13e417784b775683980d0729f63a3
Muhammet <muhammeta7@gmail.com>
2017-04-19 18:56:30 -0400

    js constructors exercises

--- objects.js
+++ objects.js
@@ -116,3 +116,169 @@ bicycle.gear = 1;
 bicycle.frame_material= "carbon fiber";
 
 console.log(bicycle)
+
+// Function review
+var square = function (x) {
+  return x * x;
+};
+
+var multiply = function(x,y) {
+    return x * y;
+}
+
+multiply(3,4);
+
+// So What's a Method
+var bob = new Object();
+bob.name = "Bob Smith";
+bob.age = 30;
+
+bob.setAge = function (newAge){
+  bob.age = newAge;
+};
+// here we set bob's age to 40
+bob.setAge(40);
+// bob's feeling old.  Use our method to set bob's age to 20
+bob.setAge(20);
+
+// Why are methods important
+var bob = new Object();
+bob.age = 17;
+// this time we have added a method, setAge
+bob.setAge = function (newAge){
+  bob.age = newAge;
+};
+
+bob.getYearOfBirth = function () {
+  return 2014 - bob.age;
+};
+
+console.log(bob.getYearOfBirth());
+
+// "this" keyword
+var setAge = function (newAge) {
+  this.age = newAge;
+};
+
+var bob = new Object();
+bob.age = 30;
+bob.setAge = setAge;
+  
+// change bob's age to 50 here
+bob.setAge(50);
+
+// This works for everyone
+var setAge = function (newAge) {
+  this.age = newAge;
+};
+
+var bob = new Object();
+bob.age = 30;
+bob.setAge = setAge;
+  
+// make susan here, and first give her an age of 25
+var susan = new Object();
+susan.age = 25;
+susan.setAge = setAge;
+
+// here, update Susan's age to 35 using the method
+susan.setAge(35);
+
+// Make your own Method
+var rectangle = new Object();
+rectangle.height = 3;
+rectangle.width = 4;
+// here is our method to set the height
+rectangle.setHeight = function (newHeight) {
+  this.height = newHeight;
+};
+// method to set height
+rectangle.setWidth = function(newWidth) {
+    this.width = newWidth;
+}
+  
+// here change the width to 8 and height to 6 using our new methods
+rectangle.setHeight(6);
+rectangle.setWidth(8);
+
+// More kinds of Methods
+var square = new Object();
+square.sideLength = 6;
+square.calcPerimeter = function() {
+  return this.sideLength * 4;
+};
+
+square.calcArea = function() {
+    return this.sideLength*this.sideLength;
+}
+
+var p = square.calcPerimeter();
+var a = square.calcArea();
+
+// Custom Constructors
+function Person(name,age) {
+  this.name = name;
+  this.age = age;
+}
+
+// Let's make bob and susan again, using our constructor
+var bob = new Person("Bob Smith", 30);
+var susan = new Person("Susan Jordan", 25);
+// help us make george, whose name is "George Washington" and age is 275
+var george = new Person("George Washington", 275);
+
+// Try it out make a custom constructor
+function Cat(age, color) {
+  this.age = age;
+  this.color = color;
+}
+
+function Dog(breed, age, name) {
+    this.name = name;
+    this.age = age;
+    this.breed = breed;
+}
+
+// More options
+function Person(name,age) {
+  this.name = name;
+  this.age = age;
+  this.species = "Homo Sapiens";
+}
+
+var sally = new Person("Sally Bowles", 39);
+var holden =  new Person("Holden Caulfield", 16);
+console.log("sally's species is " + sally.species + " and she is " + sally.age);
+console.log("holden's species is " + holden.species + " and he is " + holden.age);
+
+// Constructors with methods
+function Rectangle(height, width) {
+  this.height = height;
+  this.width = width;
+  this.calcArea = function() {
+      return this.height * this.width;
+  };
+  // put our perimeter function here!
+  this.calcPerimeter= function() {
+      return this.height*2 + this.width*2;
+  }
+}
+
+var rex = new Rectangle(7,3);
+var area = rex.calcArea();
+var perimeter = rex.calcPerimeter();
+
+// Constructors in Review
+function Rabbit(adjective) {
+    this.adjective = adjective;
+    this.describeMyself = function() {
+        console.log("I am a " + this.adjective + " rabbit");
+    };
+}
+
+// now we can easily make all of our rabbits
+var rabbit1 = new Rabbit("fluffy");
+var rabbit2 = new Rabbit("happy");
+var rabbit3 = new Rabbit("sleepy");
+
+console.log(rabbit1.describeMyself())
