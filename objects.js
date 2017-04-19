078990abfa4fac2037ade079bcc9457ea5d000be
Muhammet <muhammeta7@gmail.com>
2017-04-19 18:00:22 -0400

    intro to objects, movieReview function

--- objects.js
+++ objects.js
@@ -27,3 +27,60 @@ for ( var numbers = 1; numbers <= 20; numbers++) {
     else {
         console.log(numbers) }
 }
+
+// I have to celebrate you baby
+var getReview = function (movie) {
+    switch (movie) {
+        case 'Toy Story 2':
+            return ("Great story. Mean prospector.");
+            // break;
+        case 'Finding Nemo':
+            return ("Cool animation, and funny turtles.");
+            // break;
+        case 'The Lion King':
+            return ("Great songs.");
+            // break;
+        default:
+            return ("I don't know!");           
+    }  
+};
+
+getReview("Toy Story 2");
+
+// Make own object called me
+var Spencer = {
+  age: 22,
+  country: "United States"
+};
+
+var me = {
+    age: 27,
+    country: 'United States'
+}
+
+// Accessing properties
+var bob = {
+  name: "Bob Smith",
+  age: 30
+};
+var susan = {
+  name: "Susan Jordan",
+  age: 25
+};
+
+var name1 = bob.name;
+var age1 = bob.age;
+
+var name2 = susan.name;
+var age2 = susan.age;
+
+// Part 2
+var dog = {
+  species: "greyhound",
+  weight: 60,
+  age: 4
+};
+
+var species = dog["species"];
+var weight = dog["weight"];
+var age = dog["age"];
