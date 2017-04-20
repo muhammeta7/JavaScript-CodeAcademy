bb2b9cb388299f48a6441878238b92dec38b90e9
Muhammet <muhammeta7@gmail.com>
2017-04-19 22:48:13 -0400

    created constructors and methods for constructors

--- objects.js
+++ objects.js
@@ -366,3 +366,56 @@ var alice = new Person("Alice", 30);
 var billy = new Person("Billy", 25);
 
 console.log("The older person is " + olderAge(alice, billy));
+
+// What are Objects for
+var spencer = {
+  age: 22,
+  country: "United States"
+};
+
+// make spencer2 here with constructor notation
+var spencer2 = new Object();
+spencer2.age = 22;
+spencer2.country = "United States";
+
+// Properties
+var snoopy = new Object();
+snoopy.species = "beagle";
+snoopy.age = 10;
+
+// save Snoopy's age and species into variables
+// use dot notation for snoopy's species
+var species = snoopy.species;
+    
+// use bracket notation for snoopy's age ex: ObjectName["propertyName"]
+var age = snoopy["age"];
+
+// Customizing Constructors
+// Save us time and lines of code
+var harry_potter = new Object();
+harry_potter.pages = 350;
+harry_potter.author = "J.K. Rowling";
+
+// A custom constructor for book
+function Book (pages, author) {
+    this.pages = pages;
+    this.author = author;
+}
+
+// Use our new constructor to make the_hobbit in one line
+var the_hobbit = new Book (320, "J.R.R. Tolkien");
+
+// Methods: functions associated with object
+function Circle (radius) {
+    this.radius = radius;
+    this.area = function () {
+        return Math.PI * this.radius * this.radius;
+        
+    };
+    // define a perimeter method here
+    this.perimeter = function(radius) {
+        return 2 * Math.PI * this.radius;
+    };
+};
+
+Circle(5);
