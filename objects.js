d862bd04c0be61d77a573ab59cab54221648f108
Muhammet <muhammeta7@gmail.com>
2017-04-19 22:35:11 -0400

    make object arrays, constructor functions

--- objects.js
+++ objects.js
@@ -282,3 +282,87 @@ var rabbit2 = new Rabbit("happy");
 var rabbit3 = new Rabbit("sleepy");
 
 console.log(rabbit1.describeMyself())
+
+// Array of Objects
+function Person (name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+// Now we can make an array of people
+var family = new Array();
+family[0] = new Person("alice", 40);
+family[1] = new Person("bob", 42);
+family[2] = new Person("michelle", 8);
+// add the last family member, "timmy", who is 6 years old
+family[3] = new Person("timmy", 6);
+
+// Loop the loop
+// Our Person constructor
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+// Now we can make an array of people
+var family = new Array();
+family[0] = new Person("alice", 40);
+family[1] = new Person("bob", 42);
+family[2] = new Person("michelle", 8);
+family[3] = new Person("timmy", 6);
+
+// loop through our new array
+for ( var i=0; i<family.length; i++) {
+    console.log(family[i]);
+}
+
+// Passing objects into Functions
+// Our person constructor
+function Person (name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+// We can make a function which takes persons as arguments
+// This one computes the difference in ages between two people
+var ageDifference = function(person1, person2) {
+    return person1.age - person2.age;
+}
+
+var alice = new Person("Alice", 30);
+var billy = new Person("Billy", 25);
+
+// get the difference in age between alice and billy using our function
+var diff = ageDifference (alice, billy);
+
+console.log(diff);
+
+// Try it out
+// Our person constructor
+function Person (name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+// We can make a function which takes persons as arguments
+// This one computes the difference in ages between two people
+var ageDifference = function(person1, person2) {
+    return person1.age - person2.age;
+};
+
+// Make a new function, olderAge, to return the age of
+// the older of two people
+function olderAge (person1, person2) {
+    if ( person1.age > person2.age) {
+        return person1.age;
+    }
+    else {
+        return person2.age;
+    }
+}
+
+// Let's bring back alice and billy to test our new function
+var alice = new Person("Alice", 30);
+var billy = new Person("Billy", 25);
+
+console.log("The older person is " + olderAge(alice, billy));
