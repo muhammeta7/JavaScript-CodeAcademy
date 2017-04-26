701dd32f0933551e0d21aad8073aaaa14067aeaa
Muhammet <muhammeta7@gmail.com>
2017-04-26 18:28:36 -0400

    methods with parameters and bracket notation

--- objects2.js
+++ objects2.js
@@ -34,3 +34,47 @@ var user = new Person("Codecademy Student",false);
 user.speak();
 
 // Literally Speaking
+var james = {
+    job: "programmer",
+    married: false,
+    speak: function( mood ) {
+        console.log("Hello, I am feeling " + mood);
+    }
+};
+
+james.speak("great");
+james.speak("just okay");
+
+// Can I see your references
+var james = {
+    job: "programmer",
+    married: false,
+    sayJob: function() {
+        // complete this method
+        console.log("Hi, I work as a " + this.job);
+    }
+};
+
+// james' first job
+james.sayJob();
+
+// change james' job to "super programmer" here
+james.job = "super programmer"
+
+// james' second job
+james.sayJob();
+
+// Whats in Your Bracket
+var james = {
+    job: "programmer",
+    married: false
+};
+
+// set to the first property name of "james"
+var aProperty = "job";
+
+// print the value of the first property of "james" 
+// using the variable "aProperty"
+console.log(james[aProperty]);
+
+// I.D. Please
